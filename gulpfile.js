401cb31d6d080289faf005eed36bf406df4f151d
Michael Laccetti <michael@laccetti.com>
2016-08-22 18:25:23 -0400

    Migrating the monorepo into different projects; this is glint-client

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,45 @@
-const hub = require('gulp-hub');
+// gulp
+const gulp = require('gulp');
 
-// dashboard: 'glint-dashboard/gulpfile.js',
-const projects = {
-  library: 'glint-lib/gulpfile.js',
-  server: 'glint-server/gulpfile.js'
-};
+// plugins
+const eslint = require('gulp-eslint');
 
-hub([projects.dashboard, projects.library, projects.server]);
+// testing
+const mocha = require('gulp-mocha');
+
+// utilities
+const del = require('del');
+const runSequence = require('run-sequence');
+
+gulp.task('clean', () => {
+  return del(['dist/**']);
+});
+
+gulp.task('lint', () => {
+  return gulp.src(['src/**/*.js', 'test/**/*.js', 'gulpfile.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', ['lint'], () => {
+  return gulp.src('./test/**/*.js')
+    .pipe(mocha({
+      reporter: 'spec',
+      quiet:    false,
+      colors:   true,
+      timeout:  10000
+    }));
+});
+
+gulp.task('build', ['test'], () => {
+  // commented out, since we don't do any transpilation or concatenation or minification or whatever (yet)
+  // return gulp.src('src/**/*.js')
+  //  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', (callback) => {
+  runSequence('clean', 'build', callback);
+});
+
+gulp.task('dist', ['default']);
